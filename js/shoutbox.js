52993be95e7e18edcd6b66caa0ea4910d49020a1
Ron Marsden <ronmarsden@hotmail.co.uk>
2016-01-12 09:21:12 +0000

    0.3.2 - adds links to megalelz' firefox repository & ngu profile

--- js/shoutbox.js
+++ js/shoutbox.js
@@ -15,9 +15,17 @@ $(document).ready(function() {
 	$('#jb_link').click(function(tab){
 		chrome.tabs.create({url: 'http://www.nextgenupdate.com/forums/members/116424-jb.html'});
 	});
+
+	$('#megalelz_link').click(function(tab){
+		chrome.tabs.create({url: 'http://www.nextgenupdate.com/forums/members/796679-megalelz.html'});
+	});
 	
-	$('#github_link').click(function(tab){
+	$('#github_chrome_link').click(function(tab){
 		chrome.tabs.create({url: 'https://github.com/xijbx/ngusb-chrome'});
+	});
+
+	$('#github_firefox_link').click(function(tab){
+		chrome.tabs.create({url: 'https://github.com/MEGALELZ/ngusb'});
 	});
 });
 
