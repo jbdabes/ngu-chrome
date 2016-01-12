b11bc1ca35bf8a3660b2901ef0c5fa00e57a6d99
xijbx <jb@p0wersurge.com>
2016-01-12 20:29:44 +0000

    0.4b1 - private messages now pop into their own tab but don't close correctly, will amend in 0.4b2, also adds fontawesome css to build

--- js/shoutbox.js
+++ js/shoutbox.js
@@ -1,5 +1,8 @@
+var xhrMethod, xhrPmid;
+
 $(document).ready(function() {
 	/* grab pm list */
+	xhrMethod = 'pmlist';
 	$('#pm_input').load('http://www.nextgenupdate.com/forums/private.php #pmfolderlist');
 
 	/* fix links */
@@ -30,21 +33,52 @@ $(document).ready(function() {
 });
 
 $(document).ajaxComplete(function(event, xhr, settings) {
-	/* fix pm title links */
-	$('a.title').each(function(){
-		var href = this.href.match(/private.php\?do=showpm\&pmid=(\d+)/);
-		$(this).click(function(){
-			chrome.tabs.create({url: 'http://www.nextgenupdate.com/forums/private.php?do=showpm&pmid=' + href[1]});
+	if (xhrMethod == 'pmlist') {
+		/* fix pm title links */
+		$('a.title').each(function(){
+			var href = this.href.match(/private.php\?do=showpm\&pmid=(\d+)/);
+			var title = this.innerHTML;
+			$(this).click(function(){
+				//chrome.tabs.create({url: 'http://www.nextgenupdate.com/forums/private.php?do=showpm&pmid=' + href[1]});
+				openPrivateMessage(href[1], title);
+			});
+			this.href = '#';
 		});
-		this.href = '#';
-	});
 
-	/* fix pm sender links */
-	$('a.username.understate').each(function(){
-		var href = this.href.match(/members\/(.*)\.html/);
-		$(this).click(function(){
-			chrome.tabs.create({url: 'http://www.nextgenupdate.com/forums/members/' + href[1] + '.html'});
+		/* fix pm sender links */
+		$('a.username.understate').each(function(){
+			var href = this.href.match(/members\/(.*)\.html/);
+			$(this).click(function(){
+				chrome.tabs.create({url: 'http://www.nextgenupdate.com/forums/members/' + href[1] + '.html'});
+			});
+			this.href = '#';
 		});
-		this.href = '#';
-	});
+	} else if (xhrMethod == 'loadpm') {
+		$('#pm_' + xhrPmid + '_tab a').click();
+		/* close pm tabs */
+		$('#pm_' + xhrPmid + '_tab a i.close_pm_tab').click(function(){
+			var thisPmid = xhrPmid;
+			$('#messages-tab').click();
+			$('#pm_' + thisPmid + '_body').remove();
+			$('#pm_' + thisPmid + '_tab').remove();
+		});
+	}
 });
+
+openPrivateMessage = function(pmid, title)
+{
+	/* create tab element */
+	var newTab = "<li id=\"pm_" + pmid + "_tab\"><a data-toggle=\"tab\" href=\"#pm_" + pmid + "_body\"><i class=\"fa fa-fw fa-times close_pm_tab\" data-pmid=\"" + pmid + "\"></i><i class=\"fa fa-lg fa-fw fa-envelope-o\"></i> " + title + "</a></li>";
+	$('#tab-bar').append(newTab);
+
+	/* create tab content */
+	var newTabContent = "<div id=\"pm_" + pmid + "_body\" class=\"tab-pane fade\"><div id=\"pm_" + pmid + "_content\"></div></div>";
+	$('#tab-container').append(newTabContent);
+
+	/* load in the private message form */
+	$('#pm_' + pmid + '_content').load('http://www.nextgenupdate.com/forums/private.php?do=showpm&pmid=' + pmid + ' div#showpm');
+
+	/* auto switch to new pm tab */
+	xhrMethod = 'loadpm';
+	xhrPmid = pmid;
+}
