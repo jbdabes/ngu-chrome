6784f120b553ac9868dab4955e72112f1f89dbfd
Ron Marsden <ronmarsden@Rons-MacBook-Pro.local>
2016-01-19 02:57:58 +0000

    0.4b2 - dynamic tab closing works (kinda) - still needs tweaking

--- js/shoutbox.js
+++ js/shoutbox.js
@@ -1,4 +1,19 @@
-var xhrMethod, xhrPmid;
+var xhrMethod;
+var xhrPmid;
+var currentTab;
+
+$(function () {
+
+    //when ever any tab is clicked this method will be call
+    $("#myTab").on("click", "a", function (e) {
+        //e.preventDefault();
+
+        $(this).tab('show');
+        $currentTab = $(this);
+    });
+
+    registerCloseEvent();
+});
 
 $(document).ready(function() {
 	/* grab pm list */
@@ -30,6 +45,12 @@ $(document).ready(function() {
 	$('#github_firefox_link').click(function(tab){
 		chrome.tabs.create({url: 'https://github.com/MEGALELZ/ngusb'});
 	});
+
+	$('#ngu_thread_link').click(function(tab){
+		chrome.tabs.create({url: 'http://www.nextgenupdate.com/forums/internet-talk/880108-ngu-shoutbox-extension-chrome.html'});
+	});
+
+	registerCloseEvent();
 });
 
 $(document).ajaxComplete(function(event, xhr, settings) {
@@ -56,19 +77,20 @@ $(document).ajaxComplete(function(event, xhr, settings) {
 	} else if (xhrMethod == 'loadpm') {
 		$('#pm_' + xhrPmid + '_tab a').click();
 		/* close pm tabs */
-		$('#pm_' + xhrPmid + '_tab a i.close_pm_tab').click(function(){
-			var thisPmid = xhrPmid;
-			$('#messages-tab').click();
-			$('#pm_' + thisPmid + '_body').remove();
-			$('#pm_' + thisPmid + '_tab').remove();
-		});
+		registerCloseEvent();
+		// $('#pm_' + xhrPmid + '_tab a i.close_pm_tab').click(function(){
+		// 	var thisPmid = xhrPmid;
+		// 	$('#messages-tab').click();
+		// 	$('#pm_' + thisPmid + '_body').remove();
+		// 	$('#pm_' + thisPmid + '_tab').remove();
+		// });
 	}
 });
 
-openPrivateMessage = function(pmid, title)
+function openPrivateMessage(pmid, title)
 {
 	/* create tab element */
-	var newTab = "<li id=\"pm_" + pmid + "_tab\"><a data-toggle=\"tab\" href=\"#pm_" + pmid + "_body\"><i class=\"fa fa-fw fa-times close_pm_tab\" data-pmid=\"" + pmid + "\"></i><i class=\"fa fa-lg fa-fw fa-envelope-o\"></i> " + title + "</a></li>";
+	var newTab = "<li id=\"pm_" + pmid + "_tab\"><a data-toggle=\"tab\" href=\"#pm_" + pmid + "_body\"><button class=\"close closeTab\" data-pmid=\"" + pmid + "\" type=\"button\" >&times;</button><i class=\"fa fa-lg fa-fw fa-envelope-o\"></i> " + title + "</a></li>";
 	$('#tab-bar').append(newTab);
 
 	/* create tab content */
@@ -82,3 +104,21 @@ openPrivateMessage = function(pmid, title)
 	xhrMethod = 'loadpm';
 	xhrPmid = pmid;
 }
+
+function registerCloseEvent() {
+
+    $(".closeTab").click(function () {
+
+        //there are multiple elements which has .closeTab icon so close the tab whose close icon is clicked
+        var tabContentId = $(this).parent().attr("href");
+        $(this).parent().parent().remove(); //remove li of tab
+        $('#messages-tab a').tab('show'); // Select messages tab
+        $(tabContentId).remove(); //remove respective tab content
+
+    });
+}
+
+
+
+
+
